d2850c81825cd92fc92f313f83c171079218bf48
Melissa <kabalisamelissa2@gmail.com>
2023-12-18 09:06:35 +0200

    first commit

---
+++ src/main/java/rw/ac/rca/melissakabalisaterm1b/MelissakabalisaTerm1BApplication.java
@@ -0,0 +1,13 @@
+package rw.ac.rca.melissakabalisaterm1b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelissakabalisaTerm1BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MelissakabalisaTerm1BApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rw/ac/rca/melissakabalisaterm1b/MelissakabalisaTerm1BApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.ac.rca.melissakabalisaterm1b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelissakabalisaTerm1BApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
