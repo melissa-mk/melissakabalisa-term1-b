871e67c64c7b1ee505145fd5eb52e60858e989a4
Melissa <kabalisamelissa2@gmail.com>
2023-12-18 11:47:31 +0200

    updated tests

--- src/test/java/rw/ac/rca/melissakabalisaterm1b/endtoend/MathApplicationE2ETest.java
+++ src/test/java/rw/ac/rca/melissakabalisaterm1b/endtoend/MathApplicationE2ETest.java
@@ -1,24 +1,37 @@
 package rw.ac.rca.melissakabalisaterm1b.endtoend;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MathApplicationE2ETest {
 
-    private final TestRestTemplate restTemplate = new TestRestTemplate();
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
     public void testApiEndpoint() {
-        String url = "http://localhost:" + port + "/api/endpoint";
-        String response = restTemplate.getForObject(url, String.class);
+        // Given
+        String url = "http://localhost:" + port + "/api/v1/do_math";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String requestBody = "{\"operand1\": 2, \"operand2\": 5, \"operation\": \"+\"}";
 
+        // When
+        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+        String response = restTemplate.postForObject(url, request, String.class);
+
+        // Then
         assertEquals("Expected Response", response);
     }
 }
